206fa85c98b95a074e181c1b5d378fa7367c943d
m0nztrum <delafiado@gmail.com>
2024-11-11 20:06:51 +0000

    env for keys

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
-    'https://ujgplhkdujbnkhgrenib.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqZ3BsaGtkdWpibmtoZ3JlbmliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzMjMzNzAsImV4cCI6MjA0Njg5OTM3MH0.0p2RqRoO8YXoOGloxJsOIm6GZ6BUjKRmsjmzAzwDpYM',
+    import.meta.env.VITE_SUPABASE_URL as string,
+    import.meta.env.VITE_SUPABASE_ANON_KEY as string,
 );
 
 export { supabase };
