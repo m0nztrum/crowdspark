a9975ad8be8dff614e4c6aa2ad7e641b109bbfc8
m0nztrum <delafiado@gmail.com>
2024-11-13 17:25:31 +0000

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { supabase } from '@/services/supabase';
-import { useState, useEffect } from 'react';
-
 export const App = () => {
-    const [countries, setCountries] = useState([]);
-
-    useEffect(() => {
-        getCountries();
-    }, []);
-
-    async function getCountries() {
-        const { data } = await supabase.from('countries').select();
-        setCountries(data);
-        console.log(data, 'RESPONSE');
-    }
     return (
         <main>
-            <h1 className="text-center text-3xl">Countries</h1>
-            <ul className="grid grid-cols-3 gap-2">
-                {countries.map((country) => (
-                    <li className="border border-black p-2" key={country.name}>
-                        {country.name}
-                    </li>
-                ))}
-            </ul>
+            <h1 className="text-center text-3xl">Crowdspark</h1>
         </main>
     );
 };
